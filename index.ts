a214c79c08a2f918fd3c204edd04604f98085b59
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2024-04-03 11:26:43 -0400

    Implement basic feedback form

---
+++ index.ts
@@ -0,0 +1,64 @@
+import { nodes } from "membrane";
+
+interface Req {
+  body: string;
+  method: string;
+}
+
+export async function endpoint(req: Req) {
+  switch (req.method) {
+    case "POST":
+      return await handleFormSubmission(JSON.parse(req.body));
+    case "GET":
+      return await serveFeedbackForm();
+    default:
+      return JSON.stringify({
+        status: 405,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: "method not allowed" }),
+      });
+  }
+}
+
+interface FeedbackForm {
+  email?: string;
+  feedback: string;
+  url: string;
+}
+
+async function handleFormSubmission(form: FeedbackForm) {
+  const { email, feedback, url } = form;
+
+  const subject = `Garden thoughts from ${email ?? "anonymous reader"}`;
+  const body = `Thoughts on ${url}:\n${feedback}`;
+  await nodes.email.send({ subject, body }).$invoke();
+
+  return JSON.stringify({
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: "feedback received!" }),
+  });
+}
+
+async function serveFeedbackForm() {
+  return JSON.stringify({
+    status: 200,
+    headers: { "Content-Type": "text/html" },
+    body: `
+      <html>
+        <body>
+          <h1>Blog feedback form</h1>
+          <p>This form submits feedback to a membrane program I wrote for petemillspaugh.com.</p>
+          <p>The code is public, and I posted a blog post and Tweet demo</p>
+          <form method="POST" action="https://studio-878-reserve-690-drawer-792-hat.hook.membrane.io">
+            <label for="email">Your email:</label><br>
+            <input type="email" id="email" name="email"><br><br>
+            <label for="feedback">Your thoughts:</label><br>
+            <textarea id="feedback" name="feedback" required></textarea><br><br>
+            <input type="submit" value="Submit">
+          </form>
+        </body>
+      </html>
+    `,
+  });
+}
